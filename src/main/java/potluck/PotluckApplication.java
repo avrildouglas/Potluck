0e6f96a8042a7b2001edf46bd8ded6d7e4e75414
avrildouglas <adougla2@montgomerycollege.edu>
2017-06-27 08:18:15 -0400

    third commit

--- src/main/java/potluck/PotluckApplication.java
+++ src/main/java/potluck/PotluckApplication.java
@@ -2,8 +2,10 @@ package potluck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=PotluckMainController.class)
 public class PotluckApplication {
 
 	public static void main(String[] args) {
